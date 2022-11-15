17dc641122140e254524e29acbd783ea39ba0f26
Marcin Zepp <nircek-2103@protonmail.com>
2022-11-16 00:12:33 +0100

    Add localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
 import './App.css'
 import NewReceiptForm from './NewReceiptForm';
 import Receipt from './Receipt';
+import { useLocalStorage } from './utils';
 
+const defaultValue = [
+  { id: 1, date: "2020-03-30", where: "Żabka", list: [{ what: "Apple", amount: 3, price: 1.00 }] },
+  { id: 2, date: "2020-03-31", where: "Biedronka", list: [{ what: "Apple", amount: 15, price: 0.50 }] }
+];
 export default function App() {
-  const [receipts, setReceipts] = useState([
-    { id: 1, date: "2020-03-30", where: "Żabka", list: [{ what: "Apple", amount: 3, price: 1.00 }] },
-    { id: 2, date: "2020-03-31", where: "Biedronka", list: [{ what: "Apple", amount: 15, price: 0.50 }] }
-  ]);
+  const [receipts, setReceipts] = useLocalStorage('receipts', defaultValue);
 
   const addNewReceipt = (details) => {
     setReceipts(r => [...r, {
@@ -19,6 +20,7 @@ export default function App() {
   const setList = (id) => (mutate) => setReceipts(r => r.map(x => x.id != id ? x : { ...x, list: mutate(x.list) }))
 
   return <>
+    <button onClick={() => setReceipts(defaultValue)}> Clear </button>
     {receipts.map(x => <Receipt key={x.id} setList={setList(x.id)} {...x} />)}
     <NewReceiptForm {...{ addNewReceipt }} />
   </>
--- src/utils.jsx
+++ src/utils.jsx
@@ -1,2 +1,17 @@
+import { useEffect, useState } from "react"
+
 export const width = (ch) => ({ style: { width: ch + 'ch' } })
 export const floatify = (x) => +x == parseFloat(x) ? +x : NaN;
+
+const LS_PREFIX = "io.github.nircek.expenses-db."
+export function useLocalStorage(key, initialValue = null) {
+    key = LS_PREFIX + key
+    const [value, setValue] = useState(() => {
+        const json = localStorage.getItem(key)
+        if (json !== null) return JSON.parse(json)
+        if (typeof initialValue === "function") initialValue = initialValue()
+        return initialValue
+    })
+    useEffect(() => localStorage.setItem(key, JSON.stringify(value)), [key, value])
+    return [value, setValue]
+}
